079a54dc45f918541168bf397917770d91bc34c2
Your Name <your.email@example.com>
2025-10-20 09:48:20 +0200

    chore: trigger fresh deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	// Trigger new build
 	site: 'https://blog.thevaluationgenie.com',
 	integrations: [
 		mdx(), 
